02008251507d24dcfee6db3952f6a3fecca81b95
Shilcof <robert.shilcof@gmail.com>
2020-12-17 18:30:39 +0000

    commit tree 34315cea68f8849422d1b74078853c0cd21424cc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :summary, length: { maximum: 250 }
+    validates :content, length: { minimum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-fiction) }
+    validates_each :title do |record, attr, value|
+        record.errors.add(attr, "must be clickbaity") if value && !value.match(/(Won't Believe)|(Secret)|(Top \d*)|(Guess)/)
+    end
 end
